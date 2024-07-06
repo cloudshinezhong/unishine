e1641f364036b843a871a27bbe848189f6b351cb
Zhong <15180284387@163.com>
2024-07-07 05:37:40 +0800

    feat: 添加关于项目

--- src/pages/my/index.vue
+++ src/pages/my/index.vue
@@ -31,6 +31,7 @@
           clickable
           :note="`最近登录: ${dayjs(userInfo.login_ts).format('YYYY/MM/DD HH:mm:ss')}`"
         ></uni-list-item>
+        <uni-list-item clickable link note="关于项目" @click="declareUser"></uni-list-item>
       </uni-list>
     </view>
     <view class="flex items-center flex-col leading-6" v-else @click="show = true">
@@ -41,6 +42,51 @@
       <button type="warn" v-if="hasLogin" class="w-full" @click="logout">退出登录</button>
     </view>
   </view>
+  <!-- 底部弹窗 -->
+  <uni-popup ref="myTopPopup" :safe-area="false">
+    <view style="background-color: #fff" class="max-h-88 min-h-38">
+      <view class="flex center p-2 bg-white">
+        <!--   占位元素     -->
+        <view class="" style="opacity: 0"><uni-icons type="closeempty" size="24"></uni-icons></view>
+        <view class="flex-1 center font-400"><text style="font-size: 18px">关于项目</text></view>
+        <view @click.stop="() => myTopPopup.close()">
+          <uni-icons type="closeempty" size="24"></uni-icons>
+        </view>
+      </view>
+      <scroll-view scroll-y class="p-2 w-full" style="box-sizing: border-box">
+        <view>
+          <text font-size="3">最新打包时间：{{ buildDate }}</text>
+        </view>
+        <view>
+          <text font-size="3" word-spacing="break">
+            github地址(未开源)：
+            <uni-link
+              href="https://github.com/cloudshinezhong/unishine"
+              text="https://github.com/cloudshinezhong/unishine"
+            ></uni-link>
+          </text>
+        </view>
+        <view>
+          <text font-size="3">作者：shinezhong</text>
+        </view>
+        <view>
+          <text font-size="3">联系：cloudshinezhong@gmail.com</text>
+        </view>
+        <view>
+          <text font-size="3">
+            关于：项目为实用AI功能集，相关功能还在完善，未开源原因， 1.项目还不完善，
+            2.项目可能会被换皮商用，3.其他原因。项目当前部署在边缘服务节点，AI服务在某些地区不可用，中转了一层代理导致访问较缓慢，望谅解。
+          </text>
+        </view>
+        <view>
+          <text font-size="3">
+            注意：由于项目部署为云部署(为海外云服务厂商)，个人项目无资金支持，无法去使用付费套餐内容，免费套餐存在一些限制，例如宽带，请求并发数，数据库，缓存，对象存储等。望酌情使用，可联系作者赞助，以更好支持项目。
+          </text>
+        </view>
+        <view class="mb-10"></view>
+      </scroll-view>
+    </view>
+  </uni-popup>
 </template>
 
 <script lang="ts" setup>
@@ -51,11 +97,20 @@ import { userLogout } from '@/service/user'
 const mailUserStore = useMailUserStore()
 
 const show = ref(false)
+
+const myTopPopup = ref(null) // popup组件
+
+const buildDate = ref('')
+
 const hasLogin = mailUserStore.isLogin
 const userInfo = mailUserStore.mailUserInfo
 
 function gotoUserInfo() {
   return uni.showToast({ icon: 'none', title: `相关功能正在开发中...` })
+}
+
+function declareUser() {
+  myTopPopup.value.open('top')
 }
 
 function setClipboardData(data: string) {
@@ -89,4 +144,8 @@ const logout = () => {
     },
   })
 }
+
+onMounted(() => {
+  buildDate.value = document.getElementsByTagName('html')[0].getAttribute('build-date')
+})
 </script>
